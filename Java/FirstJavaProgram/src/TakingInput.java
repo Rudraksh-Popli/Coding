bbfc1b018a298fb870f166b25539a895aed76852
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-06-14 18:50:25 +0530

    Added Code for Taking User Input in Java

--- Java/FirstJavaProgram/src/TakingInput.java
+++ Java/FirstJavaProgram/src/TakingInput.java
@@ -4,24 +4,31 @@ public class TakingInput
 {
     public static void main(String[] args)
     {
-        System.out.print("Taking Input From the User : ");
         Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter number 1");
-        // int a = sc.nextInt();
-        // float a = sc.nextFloat();
-        // System.out.println("Enter number 2");
-        // int b = sc.nextInt();
-        // float b = sc.nextFloat();
 
-        // int sum = a + b;
-        // float sum = a + b;
-        // System.out.println("The sum of these numbers is");
-        // System.out.println(sum);
-        // boolean b1 = sc.hasNextInt();
-        // System.out.println(b1);
-        // String str = sc.next();
+        /*
+        System.out.print("Taking Input From the User : ");
         String str = sc.nextLine();
-        System.out.print(str);
+        System.out.println(str);
 
+        System.out.print("Enter number 1 : ");
+        // int a = sc.nextInt();
+        float a = sc.nextFloat();
+        System.out.print("Enter number 2 : ");
+        // int b = sc.nextInt();
+        float b = sc.nextFloat();
+        // int sum = a + b;
+        float sum = a + b;
+        System.out.print("The sum of these numbers is : ");
+        System.out.print(sum);
+
+        boolean b1 = sc.hasNextInt();
+        System.out.println(b1);
+         */
+
+        //String str1 = sc.next();
+
+        String str2 = sc.nextLine();
+        System.out.print(str2);
     }
 }
