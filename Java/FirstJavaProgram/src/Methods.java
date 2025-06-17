41f0e8aa079d625ce46e2eb7c9d95ec9aecb1829
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-06-17 19:51:53 +0530

    Added Code for Methods in Java

--- Java/FirstJavaProgram/src/Methods.java
+++ Java/FirstJavaProgram/src/Methods.java
@@ -15,6 +15,19 @@ public class Methods
 		return z;
 	}
 	
+	static void change(int a)
+	{
+		a = 98;
+	}
+	static void change2(int [] arr)
+	{
+		arr[0] = 98;
+	}
+	static void tellJoke()
+	{
+		System.out.println("I invented a new word!\nPlagiarism!");
+	}
+	
 	// Method Overloading
 	
 	static void foo()
@@ -35,18 +48,6 @@ public class Methods
 		System.out.println("Good morning " + a + " bro!");
 		System.out.println("Good morning " + b + " bro!");
 	}
-	static void change(int a)
-	{
-		a = 98;
-	}
-	static void change2(int [] arr)
-	{
-		arr[0] = 98;
-	}
-	static void tellJoke()
-	{
-		System.out.println("I invented a new word!\nPlagiarism!");
-	}
 	
 	public static void main(String[] args)
 	{
@@ -65,9 +66,7 @@ public class Methods
 		c1 = logic(a1, b1);
 		System.out.println(c);
 		System.out.println(c1);
-		
-		// Method Overloading
-		
+
 		tellJoke();
 		// Case 1: Changing the Integer
 		int x = 45;
@@ -77,7 +76,9 @@ public class Methods
 		int [] marks = {52, 73, 77, 89, 98, 94};
 		change2(marks);
 		System.out.println("The value of x after running change is : " + marks[0]);
+		
 		// Method Overloading
+		
 		foo();
 		foo(3000);
 		foo(3000, 4000);
