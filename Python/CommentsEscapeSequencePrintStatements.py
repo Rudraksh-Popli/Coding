02b0d78dd5aca56034c73e17be3fbbc80895731b
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-07-03 19:36:13 +0530

    Added Code for Comments, Escape Sequence Characters and Print Statements in Python

---
+++ Python/CommentsEscapeSequencePrintStatements.py
@@ -0,0 +1,21 @@
+'''
+Hey, Please dont remove this line
+Author: Rudraksh Popli
+Course: 100 days of Code
+'''
+"""
+Hey, Please dont remove this line
+Author: Rudraksh Popli
+Course: 100 days of Code
+"""
+# Above, is a multi-line comment
+
+print("Hey, I am a \"good boy\"\nand this viewer is also a good boy/girl")
+
+# # Hey, Please dont remove this line
+# # This is a single-line comment
+# print("This is a print statement")
+# print("Hello World") #Printing Hello World
+
+print("Hey",6,7,sep="~",end="007\n")
+print("RP3126")
