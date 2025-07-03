1421e91edda03341b91afad147bd020df024f8f4
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-07-03 19:56:28 +0530

    Added Code for Variables and Data Types in Python

---
+++ Python/Variables&DataTypes.py
@@ -0,0 +1,26 @@
+a=1123154356764745234
+print(a)
+b="Rudraksh"
+print(b)
+c=complex(2,3)
+print(c)
+d=True
+print(d)
+e=None
+print(e)
+a1=9
+print(a+a1)
+print("The type of a is",type(a))
+print("The type of a is",type(b))
+print("The type of a is",type(d))
+print("The type of a is",type(e))
+print("The type of a is",type(a1))
+list1 = ["apple",1,True,"mango",None,complex(4,5)]
+print(list1)
+print("The type of a is",type(list1))
+tuple1 = ("banana",5,False,"grapes",None,complex(6,9))
+print(tuple1)
+print("The type of a is",type(tuple1))
+dict1 = {"name":"Sakshi","age":56,"canVote":False}
+print(dict1)
+print("The type of a is",type(dict1))
