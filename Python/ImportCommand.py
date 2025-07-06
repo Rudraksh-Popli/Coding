0f34d51b3caee434b400d4fb88f3228020fc71a5
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-07-06 12:21:47 +0530

    Added Code for How Import Works in Python

---
+++ Python/ImportCommand.py
@@ -0,0 +1,19 @@
+# from math import sqrt, pi
+# from math import pi, sqrt as s
+# from math import *
+# import math as math_builtin_python
+
+# result = math_builtin_python.sqrt(9) * math_builtin_python.pi
+# print(result)  # Output: 3.0
+
+# print(dir(math))
+
+# from rudraksh import welcome, rudraksh
+
+import rudraksh as rp
+import math
+
+print(dir(math))
+print(math.nan, type(math.nan))
+rp.welcome()
+print(rp.rudraksh)
---
+++ Python/rudraksh.py
@@ -0,0 +1,5 @@
+def welcome():
+  print("Hey you are welcome my friend")
+
+
+rudraksh = "A good boy"
