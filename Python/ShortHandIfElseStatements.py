5d56b4e5310dc5949f674bd4ca6d21fc08aa1eca
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-07-06 08:56:21 +0530

    Added Code for Short Hand If-Else Statements in Python

---
+++ Python/ShortHandIfElseStatements.py
@@ -0,0 +1,14 @@
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+a = 330000
+b = 3303
+print("A") if a > b else print("=") if a == b else print("B")
+
+c = 9 if a>b else 0
+print(c)
