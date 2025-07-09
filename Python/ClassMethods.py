97303f2a54c3c59cab4bd27c7d28c7b151f28e46
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-07-09 14:48:42 +0530

    Added Code for Class Methods as Alternative Constructors in Python

--- Python/ClassMethods.py
+++ Python/ClassMethods.py
@@ -1,4 +1,4 @@
-class Employee:
+class Employee1:
   name = "RP"
   company = "Apple"
   def show(self):
@@ -8,9 +8,40 @@ class Employee:
   def changeCompany(cls, newCompany):
     cls.company = newCompany
 
-e1 = Employee()
+e1 = Employee1()
 e1.name = "Rudraksh"
 e1.show()
 e1.changeCompany("Tesla")
 e1.show()
-print(Employee.company)
+print(Employee1.company)
+
+class Employee:
+  def __init__(self, name, salary):
+    self.name = name 
+    self.salary = salary
+    
+  @classmethod
+  def fromStr(cls, string):
+    return cls(string.split("-")[0], int(string.split("-")[1]))
+    
+e1 = Employee("Harry", 12000)
+print(e1.name)
+print(e1.salary)
+
+string = "John-12000"
+e2 = Employee.fromStr(string)
+print(e2.name)
+print(e2.salary)
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @classmethod
+    def from_string(cls, string):
+        name, age = string.split(',')
+        return cls(name, int(age))
+
+person = Person.from_string("John Doe, 30")
+print(person.name, person.age)
