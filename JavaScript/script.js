2e1ebbe811ef1ac90c5868a87c31e55ed8a5ca3a
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-07-17 19:12:17 +0530

    Added Code for JavaScript in Browser

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,2 +1,32 @@
 alert("Hello")
 console.log("Hey harry")
+console.log("Hello")
+let a = 4;
+let b = 2;
+console.log(a + b)
+console.log("log")
+console.info("info")
+console.warn("warn")
+console.error("err")
+console.assert("err" != false)
+console.assert("err" == false)
+
+console.time("forLoop")
+
+for (let i = 0; i < 500; i++)
+{
+  console.log(233)
+}
+
+console.timeEnd("forLoop")
+
+console.time("whileLoop")
+
+let i = 0;
+while (i < 500)
+{
+  console.log(233)
+  i++;
+}
+
+console.timeEnd("whileLoop")
