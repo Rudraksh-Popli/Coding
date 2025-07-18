43951eb11b9a8d9d8e862be0dbd7b8cea4340acd
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-07-18 11:51:23 +0530

    Added Code for alert, prompt, and confirm in JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,32 +1,48 @@
-alert("Hello")
-console.log("Hey harry")
-console.log("Hello")
-let a = 4;
-let b = 2;
-console.log(a + b)
-console.log("log")
-console.info("info")
-console.warn("warn")
-console.error("err")
-console.assert("err" != false)
-console.assert("err" == false)
+// alert("Hello")
+// console.log("Hey harry")
 
-console.time("forLoop")
+// console.log("Hello")
+// let a = 4;
+// let b = 2;
+// console.log(a + b)
+// console.log("log")
+// console.info("info")
+// console.warn("warn")
+// console.error("err")
+// console.assert("err" != false)
+// console.assert("err" == false)
 
-for (let i = 0; i < 500; i++)
+// console.time("forLoop")
+
+// for (let i = 0; i < 500; i++)
+// {
+//   console.log(233)
+// }
+
+// console.timeEnd("forLoop")
+
+// console.time("whileLoop")
+
+// let i = 0;
+// while (i < 500)
+// {
+//   console.log(233)
+//   i++;
+// }
+
+// console.timeEnd("whileLoop")
+
+alert("Your Script Works")
+alert("Enter the value of a!")
+let a = prompt("Enter a here")
+a = Number.parseInt(a)
+alert("You entered a of type " + (typeof a))
+let write = confirm("Do you want to write it to the page")
+if (write)
 {
-  console.log(233)
+  document.write(a)
 }
-
-console.timeEnd("forLoop")
-
-console.time("whileLoop")
-
-let i = 0;
-while (i < 500)
+else
 {
-  console.log(233)
-  i++;
+  document.write("Please allow me to write")
 }
-
-console.timeEnd("whileLoop")
