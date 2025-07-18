b0f8a6d0e835e7d1a5e2b1b99367db13c6a23de2
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-07-18 14:06:52 +0530

    Added Code for alert, prompt, and confirm in JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -34,7 +34,7 @@
 
 alert("Your Script Works")
 alert("Enter the value of a!")
-let a = prompt("Enter a here")
+let a = prompt("Enter a here", "578")
 a = Number.parseInt(a)
 alert("You entered a of type " + (typeof a))
 let write = confirm("Do you want to write it to the page")
