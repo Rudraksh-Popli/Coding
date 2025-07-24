c942bf11ad9c35ff2b5a216b4c781833f51071a3
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-07-24 16:25:49 +0530

    Added Code for Table Navigation in JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -62,11 +62,20 @@
 // console.log(a.parentElement)
 // console.log(a.firstChild.nextSibling)
 
-const changeBgRed = () =>
-{
-  document.body.firstElementChild.style.background = "red"
-}
+// const changeBgRed = () =>
+// {
+//   document.body.firstElementChild.style.background = "red"
+// }
 
-let b = document.body
-console.log("First child of b is: ", b.firstChild)
-console.log("First Element child of b is: ", b.firstElementChild)
+// let b = document.body
+// console.log("First child of b is: ", b.firstChild)
+// console.log("First Element child of b is: ", b.firstElementChild)
+
+let t = document.body.firstElementChild.firstElementChild
+console.log(t)
+console.log(t.rows)
+console.log(t.caption)
+console.log(t.tHead.firstElementChild)
+console.log(t.tFoot)
+console.log(t.tBodies)
+console.log(t.rows[1].rowIndex)
