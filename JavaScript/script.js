2ad4a9e6097b7fbc4555cd11d52ce1e8c88b21fe
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-12-25 15:35:01 +0530

    Added Code for Solution of Chapter 1 Practice Questions

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,21 +1,45 @@
-// nn bb ss u - Primitive Data Types in JavaScript
-let a = null;
-let b = 345;
-let c = true;
-let d = BigInt("567") + BigInt("3");
-let e = "Rudraksh";
-let f = Symbol("This is a symbol");
-let g;
-let h = undefined;
-console.log(a, b, c, d, e, f, g, h);
+// 1. Create a variable of type string and try to add a number to it
+
+let a = "Rudraksh";
+let b = 1412;
+let c = a+b;
+console.log(c);
+
+// 2. Use typeof operator to find the datatype of the string in the last question
+
 console.log(typeof c);
 
-// Objects - Non Primitive Data Type in JavaScript
+// 3. Create a const object in JavaScript. Can you change it to hold a number later
+
 const item =
 {
-    "Rudraksh": true,
-    "Aarush": false,
-    "John": 5,
-    "Charles": undefined
+    "Rudraksh" : 96,
+    "Aarush" : true,
+    "Charles" : false,
+    "John" : null
 };
-console.log(item["John"]);
+
+// item = 6; NO
+
+// 4. Try to add a new key to the const object in Problem 3. Were you able to do it ?
+
+item["Lewis"] = "Hamilton";
+item["Charles"] = "Leclerc";
+console.log(item); // YES
+
+// 5. Write a JavaScript program to create a word-meaning dictionary of 5 words
+
+const dict =
+{
+    "Avert" : "Avoid",
+    "Bestow" : "Give",
+    "Customary" : "Tradition",
+    "Decline" : "Reject",
+    "Furious" : "Angry"
+};
+
+console.log("Meaning of Avert is", dict["Avert"]);
+console.log("Meaning of Bestow is", dict.Bestow);
+console.log("Meaning of Customary is", dict["Customary"]);
+console.log("Meaning of Decline is", dict["Decline"]);
+console.log("Meaning of Furious is", dict["Furious"]);
