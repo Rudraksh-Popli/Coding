167fcf3c1e8c15da85e41dd5c59d896e493eaacb
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-12-25 13:50:11 +0530

    Added Code for let, const and var in JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,13 @@
-let c = 1107;
-console.log(c);
-c = "Rudraksh";
-console.log(c);
+// var a = 45;
+// var a = "p";
+let b = "Rudraksh";
+const author = "Rudraksh";
+b = 4;
+const Rudraksh = 0;
+let c = null;
+let d = undefined;
+{
+    let b = 'this';
+    console.log(b);
+}
+console.log(b);
