af23379626202a69c43aa5ef6fbb974d82af45bf
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-12-26 21:50:49 +0530

    Added Code for Loops in JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,54 +1,46 @@
-// 1. Use logical operators to find whether the age of a person lies between 10 and 20.
-let a = prompt("Enter your age");
-a = parseInt(a);
-if (a > 10 && a < 20) {
-    prompt("yes");
-}
-else {
-    prompt("no");
-}
+// let sum = 0;
+// let n = prompt("Enter a number");
+// n = Number.parseInt(n);
+// for (let i = 1; i <= n; i++) {
+//     sum += i;
+// }
+// alert("The sum of first " + n + " natural numbers is " + sum);
 
-// 2. Demonstrate the use of switch case statements in JavaScript.
-let b = prompt("Hey ! What's your favourite fruit ?");
-switch (b) {
-    case "Oranges":
-        alert("Oranges are $0.59 a pound.");
-        break;
-    case "Mangoes":
-    case "Papayas":
-        alert("Mangoes and papayas are $2.79 a pound.");
-        break;
-    default:
-        alert("Out of Stock");
-}
+// let fact = 1;
+// let m = prompt("Enter a number");
+// m = Number.parseInt(m);
+// for (let j = 1; j <= m; j++) {
+//     fact *= j;
+// }
+// alert("The factorial of " + m + " is " + fact);
 
-// 3. Write a JavaScript program to find whether a number is divisible by 2 and 3.
-let c = prompt("Enter a number");
-c = parseInt(c);
-if (c % 6) {
-    prompt("Number is not divisible by 2 and 3");
-}
-else {
-    prompt("Number is divisible by 2 and 3");
-}
+// let a =
+// {
+//     "Rudraksh": "Popli",
+//     "Aarush": "Popli",
+//     "Charles": "Leclerc",
+//     "Lewis": "Hamilton"
+// };
+// for (let k in a) {
+//     console.log("Surname of " + k + " is " + a[k]);
+// }
 
-// 4. Write a JavaScript program to find whether a number is divisible by 2 or 3.
-let d = prompt("Enter a number");
-d = parseInt(d);
-if (d % 6 == 0) {
-    prompt("Number is divisible by both 2 and 3");
-}
-else if (d % 2 == 0) {
-    prompt("Number is only divisible by 2");
-}
-else if (d % 3 == 0) {
-    prompt("Number is only divisible by 3");
-}
-else {
-    prompt("Number is neither divisible by 2 nor 3");
-}
+// for (let l of "Rudraksh Popli") {
+//     console.log(l);
+// }
 
-// 5. Print "You can drive" or "You cannot drive" based on age being greater than 18 using ternary operator.
-let e = prompt("Enter Age");
-e = Number.parseInt(e);
-alert("You can " + (e < 18 ? "not drive" : "drive"));
+// let n = prompt("Enter a number");
+// n = Number.parseInt(n);
+// while (n >= 0) {
+//     console.log(n);
+//     n--;
+// }
+
+let n = prompt("Enter a number");
+n = Number.parseInt(n);
+
+let i = 10;
+do {
+    console.log(i);
+    i++;
+} while (i < n);
