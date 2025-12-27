388dd08c78f98dccbe8203940f3fba9931824696
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-12-27 23:24:29 +0530

    Added Code for Solution of Chapter 4 Practice Questions

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,35 +1,37 @@
-let name1 = "Rudraksh";
-console.log(name1.length);
-for (let i = 0; i < name1.length; i++) {
-    console.log(name1[i]);
-}
-let name2 = "Aarush";
-// Template Literals
-console.log(`${name1} is a "friend" of ${name2}.`); // String Interpolation
-let fruit = "Bana\"na";
-console.log(fruit, fruit.length);
-fruit = 'Bana\'na';
-console.log(fruit, fruit.length);
-fruit = `Bana\`na`;
-console.log(fruit, fruit.length);
-fruit = `Bana\nna`;
-console.log(fruit, fruit.length);
-fruit = `Bana\tna`;
-console.log(fruit, fruit.length);
-fruit = `Bana\rna`;
-console.log(fruit, fruit.length);
+// 1. What will the following print in JavaScript ?
+console.log("har\"".length);
+// Prints 4
 
-console.log(name1.toUpperCase());
-console.log(name1.toLowerCase());
-console.log(name1.slice(2,4));
-console.log(name1.slice(2));
-let dog = "Big Dog";
-let newdog = dog.replace("Big","Small");
-console.log(newdog);
-let a1 = "This";
-let a2 = "Exists";
-let a3 = a1.concat(a2," Now");
-console.log(a3);
-let oldName = "                         Rudraksh        "
-let newName = oldName.trim();
-console.log(newName);
+// 2. Explore the includes, startsWith and endsWith function of a string.
+let a = "Rudraksh Popli is a novice programmer";
+console.log(a.endsWith("gamer"));
+console.log(a.endsWith("r"));
+console.log(a.endsWith("e"));
+console.log(a.endsWith("mer"));
+console.log(a.endsWith(" novice programmer"));
+console.log(a.startsWith("gamer"));
+console.log(a.startsWith("r"));
+console.log(a.startsWith("e"));
+console.log(a.startsWith("Rud"));
+console.log(a.startsWith("Rudraksh Popli is a "));
+console.log(a.includes(" "));
+console.log(a.includes("n"));
+console.log(a.includes("i i"));
+console.log(a.includes("Novice"));
+console.log(a.includes("ram"));
+
+// 3. Write a program to convert a given string to lowercase.
+const lowerCase = (x) => {
+    return x.toLowerCase();
+}
+console.log(lowerCase("John"));
+
+// 4. Extract the amount out of this string - "Please give Rs 1000"
+let str1 = "Please give Rs 1000";
+let amount = Number.parseInt(str1.slice("Please give Rs ".length));
+console.log(amount, typeof amount);
+
+// 5. Try to change 4th character of a given string.
+let str2 = "These are bunch of words";
+str2[3] = "K";
+console.log(str2); // Prints the unchanged given string as strings are immutable
