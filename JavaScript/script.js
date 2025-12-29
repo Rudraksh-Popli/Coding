a79f9148777f2abb4326cb15ed09fde6901b5446
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-12-29 12:25:38 +0530

    Added Code for Map, Filter and Reduce Methods in JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,24 +1,19 @@
-let num = [3, 5, 1, 2, 4];
-for (let i = 0; i < num.length; i++) {
-    console.log(num[i]);
-}
-
-num.forEach((element) => {
-    console.log(element * element);
+let arr1 = [45, 23, 31, 78, 65];
+let a1 = arr1.map((value, index, array) => {
+    console.log(value, index, array);
+    return value ** index;
 })
+console.log(a1);
 
-let name1 = "Rudraksh";
-let arrOfNum1 = Array.from(name1);
-console.log(arrOfNum1);
+let arr2 = [45, 56, 50, 70, 84, 5, 10, 12];
+let a2 = arr2.filter((value) => {
+    return value < 50;
+})
+console.log(a2);
 
-for (let i of num) {
-    console.log(i);
+let arr3 = [14, 12, 2006, 11, 7, 2007, 14, 2, 2001];
+const reduce_func = (value1, value2) => {
+    return value1 + value2;
 }
-
-for (let i in num) {
-    console.log(i);
-}
-
-for (let i in num) {
-    console.log(num[i]);
-}
+let a3 = arr3.reduce(reduce_func);
+console.log(a3);
