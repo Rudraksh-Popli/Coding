27df6a96aa530ccd338443d493b8720c0f619c89
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-12-30 15:34:33 +0530

    Added Code for console Objects in JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,25 @@
-alert("Hello");
-a=6;
-b=7;
-console.log(a+b);
+// console.log(console);
+// console.assert(3 > 5);
+// console.assert(3 < 5);
+// console.error("This is an error");
+// let obj = { 1: 3, 2: 4, 3: 5, 4: 6 };
+// console.table(obj);
+// console.warn("Tread Carefully");
+console.log("log");
+console.info("info");
+console.warn("warn");
+console.error("err");
+console.assert("err" != false);
+console.assert("err" == false);
+console.time("p1");
+for (let i = 10; i >= 0; i--) {
+    console.log(31);
+}
+console.timeEnd("p1");
+console.time("p2");
+let i = 10;
+while (i >= 0) {
+    console.log(31);
+    i--;
+}
+console.timeEnd("p2");
