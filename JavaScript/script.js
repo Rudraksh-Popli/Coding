c15d777aca96fff051716c385cd411252c21447b
Rudraksh-Popli <poplirudraksh00@gmail.com>
2025-12-31 20:03:34 +0530

    Added Code for DOM, BOM, and Window Object in JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,13 +1,7 @@
-alert("This script is working");
-alert("Enter the value of a");
-let a = prompt("Enter the value of a here", "578");
-a = Number.parseInt(a);
-alert(`You entered "a" of type ${typeof a}.`);
-let write = confirm(`Do you want to write the values of "a" to the page ?`);
-if (write) {
-
-    document.write(a);
-}
-else {
-    document.write("Allow me to write");
-}
+// console.log(window);
+// window.console.log(window);
+// alert();
+// window.alert();
+// window.console.log(document);
+window.console.log(document.body);
+document.body.style.background = "yellow";
