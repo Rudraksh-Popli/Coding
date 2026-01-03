2ac7aff13adaf6efecf4c45c90789e5f5c7bd82b
Rudraksh-Popli <poplirudraksh00@gmail.com>
2026-01-03 21:18:47 +0530

    Added Code for Searching DOM in JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,8 +1,14 @@
-let t = document.body.firstElementChild.firstElementChild
-console.log(t)
-console.log(t.rows)
-console.log(t.caption)
-console.log(t.tHead.firstElementChild)
-console.log(t.tFoot)
-console.log(t.tBodies)
-console.log(t.rows[1].rowIndex)
+// let ctitle = document.getElementsByClassName("card-title")[0];
+// ctitle.style.color = "red";
+let ctitle = document.getElementById("firstCardTitle");
+ctitle.style.color = "red";
+let ctitles = document.querySelectorAll(".card-title");
+ctitles[0].style.color = "red";
+ctitles[1].style.color = "blue";
+ctitles[2].style.color = "green";
+console.log(ctitles);
+document.querySelector(".this").style.color = "red";
+document.querySelector(".this").style.background = "black";
+console.log(document.getElementsByTagName("a"));
+console.log(document.querySelector(".card-body").getElementsByTagName("a"));
+console.log(document.getElementsByName("search"));
